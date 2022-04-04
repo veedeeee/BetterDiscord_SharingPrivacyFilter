d3f9244a6b3da0b59236994cba27a3580dec28de
Vee Dee <veedee@outlook.jp>
2022-04-04 21:03:24 +0900

    Replying destination is now supported

--- SharingPrivacyFilter.plugin.js
+++ SharingPrivacyFilter.plugin.js
@@ -12,6 +12,7 @@ module.exports = class SharingPrivacyFilter {
     this.path = {
       mask: 'M15.83 9.81C14.7 9.7 13.69 10.38 13.46 11.5C13.46 11.84 14.81 12.29 16.05 12.29C17.29 12.29 18.41 11.5 18.41 11.28C18.41 11.05 17.63 9.93 15.83 9.81M8.18 9.81C6.38 9.93 5.59 10.94 5.59 11.27C5.59 11.5 6.82 12.29 7.95 12.29S10.54 11.84 10.54 11.5C10.31 10.38 9.19 9.7 8.18 9.81M16.95 16C15.04 16 13.8 13.75 12 13.75S8.85 16 7.05 16C4.69 16 3 13.86 3 10.04C3 7.68 3.68 7 6.71 7S10.54 8.24 12 8.24 14.36 7 17.29 7 21 7.79 21 10.04C21 13.86 19.31 16 16.95 16Z',
     };
+    this.userIds = [];
   }
   
   start() { this.addButton(); }
@@ -42,36 +43,46 @@ module.exports = class SharingPrivacyFilter {
     this.button.querySelector('svg path').setAttribute('d', this.path.mask);
     this.button.dataset.isMasked = 'none';
     toolbar.insertBefore(this.button, toolbar.querySelector('[class^="search"]'));
-
+    
     this.button.addEventListener('click', evt => {
-      const myselfId = document.querySelector(`[class^='sidebar'] [class^='panels'] [class^='avatarStack'] img`).src.split('https://cdn.discordapp.com/avatars/')[1].split('/')[0];
-      console.log('myselfId', myselfId);
-
-      const userIds = [];
       document.querySelectorAll(`li[id^='chat-messages']`).forEach(li => {
         if(li.querySelectorAll(`img[class*='avatar']`).length === 0) return;
-        const img = li.querySelector(`img[class*='avatar']`);
-        const name = li.querySelector(`[id^='message-username'] [class*='username']`);
-
-        if(false === 'original' in  img.dataset)  img.dataset.original = img.src;
-        if(false === 'original' in name.dataset) name.dataset.original = name.textContent;
-
-        // Unmask
-        if(this.button.dataset.isMasked === 'masked') {
-          img.src = img.dataset.original;
-          name.textContent = name.dataset.original;
-          return;
-        }
-
-        // Mask
-        const uid = img.dataset.userId;
-        if(userIds.indexOf(uid) === -1 && uid !== myselfId) userIds.push(uid);
-
-        const display = uid === myselfId? 'Me' : userIds.indexOf(uid) + 1;
-        img.src = `https://via.placeholder.com/80.webp?text=${display}`;
-        name.textContent = display;
+        this.toggleMaskNodes(
+          li.querySelector(`img[class*='avatar']`),
+          li.querySelector(`[id^='message-username'] [class*='username']`)
+        );
+      });
+      document.querySelectorAll(`[class*='repliedMessage']`).forEach(div => {
+        if(div.querySelectorAll(`img[class*='replyAvatar']`).length === 0) return;
+        this.toggleMaskNodes(
+          div.querySelector(`img[class*='replyAvatar']`),
+          div.querySelector(`[class*='username']`)
+        );
       });
       this.button.dataset.isMasked = this.button.dataset.isMasked === 'masked'? 'none' : 'masked';
     });
+  }
+
+  toggleMaskNodes(imgNode, nameNode) {
+    const myselfId = document.querySelector(`[class^='sidebar'] [class^='panels'] [class^='avatarStack'] img`).src.split('https://cdn.discordapp.com/avatars/')[1].split('/')[0];
+
+    if(false === 'original' in  imgNode.dataset)  imgNode.dataset.original = imgNode.src;
+    if(false === 'original' in nameNode.dataset) nameNode.dataset.original = nameNode.textContent;
+
+    // Unmask
+    if(this.button.dataset.isMasked === 'masked') {
+      imgNode.src = imgNode.dataset.original;
+      nameNode.textContent = nameNode.dataset.original;
+      return;
+    }
+
+    // Mask
+    const uid = imgNode.dataset.userId;
+    if(this.userIds.indexOf(uid) === -1 && uid !== myselfId) this.userIds.push(uid);
+
+    const displayName = uid === myselfId? 'Me' : this.userIds.indexOf(uid) + 1;
+    const imgSize = (new URL(imgNode.dataset.original)).searchParams.get('size') || 80;
+    imgNode.src = `https://via.placeholder.com/${imgSize}.webp?text=${displayName}`;
+    nameNode.textContent = (nameNode.dataset.original.substr(0, 1) === '@'? '@' : '') + displayName;
   }
 };
