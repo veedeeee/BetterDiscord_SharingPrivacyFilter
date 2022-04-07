b74f740dee5e6d98a5883f365fec7e779c190212
Vee Dee <veedee@outlook.jp>
2022-04-07 19:33:43 +0900

    1.1.0

--- SharingPrivacyFilter.plugin.js
+++ SharingPrivacyFilter.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name SharingPrivacyFilter
- * @version 1.0.2
+ * @version 1.1.0
  * @description Mask user's name and icon for safety sharing.
  * @author Vee Dee
  * @authorLink http://twitter.com/v__d__
